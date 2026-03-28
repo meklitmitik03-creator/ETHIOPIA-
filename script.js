6c22ea1219bd432d51ef4aa4d54b68a669debdd3
meklitmitik03-creator <meklitmitik03@gmail.com>
2026-03-28 19:46:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -23,3 +23,17 @@ async function explore(category) {
 function closeModal() {
     document.getElementById('exploreModal').style.display = "none";
 }
+const express = require('express');
+const path = require('path'); // Add this at the top
+const app = express();
+
+app.use(express.static('public'));
+
+// ADD THIS ROUTE BELOW
+app.get('/transport', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'transport.html'));
+});
+
+app.listen(3000, () => {
+    console.log('Server running on http://localhost:3000');
+});
